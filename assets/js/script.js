d3bafaa8ec3481200496bc0484ad94a9963be641
Gabriel Rasskin <43894452+grasskin@users.noreply.github.com>
2018-10-06 13:06:04 -0400

    Now qr
    Doctor html generates qr for handoff

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,3 +44,26 @@ function login() {
         console.log("Failure");
     }
 }
+
+function generateQr(divElement, outputURL, size = 512) {
+    // divElement: div we are filling with a qr
+    // outputURL: what we are writing to the qr
+    // size: size in pixels of qr (size*size square)
+    var qrcode = new QRCode(divElement, {
+        text: outputURL,
+        width: size,
+        height: size,
+        colorDark : "#000000",
+        colorLight : "#ffffff",
+        correctLevel : QRCode.CorrectLevel.H
+    });
+    divElement.childNodes[1].setAttribute("style", "margin: auto")
+
+    return qrcode;
+}
+
+// example usage: generateQr($("#qrcode")[0], "hi");
+
+function handoff(ims){
+    generateQr($("#qrcode")[0], ims, 320);
+}
