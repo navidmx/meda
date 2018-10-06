cd157c22852925bdef3dd141d1df12ebdd03a91f
Gabriel Rasskin <43894452+grasskin@users.noreply.github.com>
2018-10-06 13:31:50 -0400

    Firebase support
    Now working, big commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -65,5 +65,5 @@ function generateQr(divElement, outputURL, size = 512) {
 // example usage: generateQr($("#qrcode")[0], "hi");
 
 function handoff(ims) {
-    generateQr($("#qrcode")[0], window.location + "../pages/patient.html?" + ims, 320);
+    generateQr($("#qrcode")[0], window.location + "../pages/patient.html?mrn=" + ims, 320);
 }
